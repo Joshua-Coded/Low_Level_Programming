69acf5aed022909f3cbb8c4191eca7643e949472
joshbam-joshwilly <j.alana@alustudent.com>
2024-10-27 09:55:29 +0200

    some codes

---
+++ structure/docu.c
@@ -0,0 +1,19 @@
+/**
+* struct my_struct - short description
+* @a: first member
+* @b: second member
+* @c: third member
+*
+*/
+
+struct my_struct {
+int a;
+int b;
+int c;
+
+}
+
+struct my_struct d = {1,2,3};
+
+
+
