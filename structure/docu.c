33083884407ec5adf1d81d0d7a8e41a406cbe3a2
joshbam-joshwilly <j.alana@alustudent.com>
2024-10-27 09:57:08 +0200

    some codes

--- structure/docu.c
+++ structure/docu.c
@@ -1,19 +1,16 @@
 /**
-* struct my_struct - short description
-* @a: first member
-* @b: second member
-* @c: third member
-*
-*/
+ * struct my_struct - short description
+ * @a: first member
+ * @b: second member
+ * @c: third member
+ *
+ */
 
-struct my_struct {
-int a;
-int b;
-int c;
+struct my_struct
+{
+    int a;
+    int b;
+    int c;
+};
 
-}
-
-struct my_struct d = {1,2,3};
-
-
-
+struct my_struct d = {1, 2, 3};
