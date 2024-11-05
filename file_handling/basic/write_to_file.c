5ad13da9d70c9445ebbcbe1d88d20c1c03f15802
joshbam-joshwilly <j.alana@alustudent.com>
2024-11-05 21:21:35 +0200

    writing into a file

---
+++ file_handling/basic/write_to_file.c
@@ -0,0 +1,28 @@
+// simple and basic file functions.
+// fopen() for opening files
+// fclose() for closing files
+
+/******************************
+ * Author: Joshua Alana
+ * Date Created: 05/11/2024
+ * Purpose: To learn how to write into a file
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *filePtr;
+    filePtr = fopen("example1.txt", "w");
+
+    if (filePtr == NULL)
+    {
+        printf("File could not opened.\n");
+        return 1;
+    }
+
+    fprintf(filePtr, "Hello Joshua!!!\n");
+    fclose(filePtr);
+    return 0;
+}
