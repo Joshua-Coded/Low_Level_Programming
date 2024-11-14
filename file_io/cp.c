c87ed9d9f145fd2e3f3de4b5b83506214e5197e1
joshbam-joshwilly <j.alana@alustudent.com>
2024-11-14 11:57:05 +0200

    complete

--- file_io/cp.c
+++ file_io/cp.c
@@ -1,65 +1,59 @@
 #include "main.h"
 
-#define BUFFER_SIZE 1024	
+#define BUFFER_SIZE 1024
 
 /**
-* error_handling - handles errors and exits
-* @message: error message to write to stdrr
-*/
-
-void error_handling(char *message)
+ * error_handler - handles errors and exits
+ * @message: error message to write to stderr
+ */
+void error_handler(char *message)
 {
-while (*message)
-	write(STDERR_FILENO, message++, 1);
+	while (*message)
+		write(STDERR_FILENO, message++, 1);
 	exit(1);
 }
 
 /**
-* main - copies content of one file to another
-* @argc: argument count
-* @argv: argument vector
-* Return: 0 on success, other on failure
-*/
-
+ * main - copies content of one file to another
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, other on failure
+ */
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to;
-ssize_T bytes_read, bytes_written;
-char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
 
-// check arguments
-if (argc != 3)
-	error_handling("Usage: cp file_from file_to\n");
+	/* Check arguments */
+	if (argc != 3)
+		error_handler("Usage: cp file_from file_to\n");
 
-// open source file for reading
-fd_from = open(argv[1], O_RDONLY);
-if (fd_from == -1)
-	error_handling("Error: can't read from file\n");
+	/* Open source file for reading */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_handler("Error: Can't read from file\n");
 
-// open/create destination file for writing
-fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fd_to == -1)
-	error_handling("Error: can't write to file\n");
-	
-// read and write loop
+	/* Open/create destination file for writing */
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+		error_handler("Error: Can't write to file\n");
 
-while ((bytes_read == read(fd_from, buffer, BUFFER_SIZE) > 0)
-{
-bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written != bytes_read)
-	error_handling("Error: write failed\n");
+	/* Read and write loop */
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+			error_handler("Error: Write failed\n");
+	}
 
-// close files
+	/* Check for read error */
+	if (bytes_read == -1)
+		error_handler("Error: Read failed\n");
 
-if (close(fd_from) == -1 || close(fd_to) == -1)
-	error_handling("Error: can't close fd\n");
+	/* Close files */
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+		error_handler("Error: Can't close fd\n");
 
-return (0);
-
+	return (0);
 }
-
-
-
-
-}
-
--- file_io/main.h
+++ file_io/main.h
@@ -1,11 +1,12 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
-void error_handling(char *message);
+/* Function prototypes */
+void error_handler(char *message);
 ssize_t _putchar(char c);
 
-#endif
+#endif /* MAIN_H */
